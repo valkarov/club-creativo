f9e46630583ec47cc7d47da459f4483607edc5c0
Val <88056645+valkarov@users.noreply.github.com>
2025-03-30 20:04:10 -0500

    Update Program.cs

--- CreativoApiV2/CreativoApiV2/Program.cs
+++ CreativoApiV2/CreativoApiV2/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<CreativoDB>();
+builder.Services.AddDbContext<CreativoDB>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnetion")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
